ebda5ecaf921b759e97fd720e02b7342affdb6d1
Natallia Nikitsina <natiknik73@gmail.com>
2023-06-24 23:50:29 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int GetIntNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int multi(int A,int B){
+    int result = Convert.ToInt32(Math.Pow(A, B));
+    return result;
+}
+
+Console.WriteLine("input number A: ");
+int num = GetIntNumber();
+Console.WriteLine("input number B: ");
+int count = GetIntNumber();
+Console.WriteLine(multi(num, count));
+
+
